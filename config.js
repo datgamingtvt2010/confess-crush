4f0120a81f04a78c1520aa60e78b4cd9f5a82088
datgamingtvt2010 <86610345+datgamingtvt2010@users.noreply.github.com>
2026-04-09 19:39:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho Anhh Thư iuuu <3',
+    btnReply: 'Gửi cho ng tên Minh Đạtt <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu Anh Thư ngoannn nhiều nhiều 😘😘',
     messDesc: 'Tốiii nayyy, mình chss gemm với nhauu nhá',
