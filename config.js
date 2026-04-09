2f518b4aafc052059bdd07a82c486147e90ba439
datgamingtvt2010 <86610345+datgamingtvt2010@users.noreply.github.com>
2026-04-09 19:41:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy, hỳ!!!',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnYes: 'Thích lắm, thươngg lắm <33',
+    btnNo: 'Không nha, ghéttt :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại thương iuu Minh Đạttttttttttttttttttttttt  <3',
     btnReply: 'Gửi cho ng tên Minh Đạtt <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu Anh Thư ngoannn nhiều nhiều 😘😘',
     messDesc: 'Tốiii nayyy, mình chss gemm với nhauu nhá',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    btnAccept: 'Dạ chs luonng sợ dì',
+    messLink: 'hì' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
